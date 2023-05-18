611fc43739c56f49ef2b446587b9ff94027790d3
FarmBuddy2023 <133762436+FarmBuddy2023@users.noreply.github.com>
2023-05-18 22:01:40 +0530

    Update weather1.js

--- weather1.js
+++ weather1.js
@@ -1,5 +1,4 @@
 
-document.addEventListener('DOMContentLoaded', ()=>{updateCartUI()})
 const head = document.querySelector('header')
 head.style.display ='none'
 const lod = document.querySelector('#lod')
